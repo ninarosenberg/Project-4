e7b213bc53bff55919eecd47a90e0acd60f99f30
Nina Rosenberg <ninaros@umich.edu>
2020-03-20 15:19:54 -0400

    Initial Commit

---
+++ Project 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 4
+//
+//  Created by Nina Rosenberg on 3/20/20.
+//  Copyright © 2020 Nina Rosenberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
